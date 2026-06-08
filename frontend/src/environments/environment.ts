eb8dcd6c515deb28e17f171a96336f3ecaf04d2a
tariktopcagic <tarik.topcagic.7@gmail.com>
2026-06-09 00:13:23 +0200

    Update API URL to MonsterASP.NET backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://sport-booking-api-gaeebqg7bvf7g8h7.austriaeast-01.azurewebsites.net/api'
+    apiUrl: 'https://sportbookingapi.runasp.net/api'
 }
